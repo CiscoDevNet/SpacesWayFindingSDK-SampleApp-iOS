cae6a48116d7453354d9c5a0d4368d31c395096a
Bandari Hari Swaroop <bhariswa@cisco.com>
2026-03-20 17:09:49 +0530

    SDK V9

---
+++ SpacesWayFindingSDK-SampleApp-iOS/Helper/WayfindingHelper.swift
@@ -0,0 +1,75 @@
+//
+//  WayfindingHelper.swift
+//  SpacesWayfindingSample
+//
+//  Created by Cisco Spaces on 07/02/2025.
+//
+
+import UIKit
+import SpacesWayFindingApp
+
+class WayfindingHelper: NSObject {
+    
+    let spacesWayFinding = SpacesWayFinding(apiKey:<api-key>,region: <region>)
+
+    
+    
+    private var completion: ((MapWidgetViewController?, Error?) -> Void)?
+    
+    func getWidget(buildingParams: SpacesWayFindingParams, completion: ((MapWidgetViewController?, Error?) -> Void)? = nil) {
+        
+        self.completion = completion
+        
+        spacesWayFinding.selectBuilding(params: buildingParams) { [weak self] (state) in
+            switch state {
+            case .running:
+                print("Wayfinding is running")
+                self?.handleCompletion(error: nil)
+                break;
+            case .failedNoInternetConnection:
+                print("Failed: No internet")
+            case .failedValidation:
+                print("Validation failed")
+            case .off:
+                print("off")
+               // removeMapWidget(mapWidget: MapWidgetTheme)
+            default:
+                print("Current state: \(state)")
+            }
+          
+        }
+      
+    }
+}
+
+extension WayfindingHelper {
+    
+    func handleCompletion(error: Error?) {
+        OperationQueue.main.addOperation { [weak self] in
+            if let error = error {
+                self?.completion?(nil, error)
+                return
+            }
+            
+            
+            let config = MapWidgetConfiguration.defaultConfiguration()
+            config.isJoystickEnabled = true
+            config.animationType=MapAnimationType.flyOver
+            config.minimumZoomLevel=12
+            config.maximumZoomLevel=24
+            
+            
+            let theme = MapWidgetTheme(accentColor: "000000", backgroundColor:  "000000", foregroundColor: "000000", successColor: "FFFFFF", alertColor: "FFFFFF", dangerColor: "FFFFFF", infoColor: "FFFFFF")
+
+            config.theme = theme
+             let mapWidget = MapWidgetViewController(configuration: config)
+            
+            mapWidget.mapEventsHandler = CustomMapEventHandler(mapWidget: mapWidget, wayfindingHelper: self)
+             mapWidget.pathFindingEventsHandler = CustomPathFindingEventsHandler(mapWidget: mapWidget)
+             mapWidget.mapExitHandler = CustomMapExitHandler(mapWidget: mapWidget)
+            
+            self?.completion?(mapWidget, nil)
+            self?.completion = nil
+        }
+    }
+}
